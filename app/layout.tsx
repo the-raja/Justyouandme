3f41673b9a07748373d4fbf42bab6bf91b96df6c
the-raja <rrajakkumar01@gmail.com>
2026-02-01 20:06:06 +0530

    just-you-and-me

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export const metadata = {
     index: false,
     follow: false,
   },
+   icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
